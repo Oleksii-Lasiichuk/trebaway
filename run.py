535e251da1fa6deb240a4f28c89ccf2ace7ab27d
Oleksii-Lasiichuk <oleks1297@gmail.com>
2025-04-14 10:41:36 +0300

    now it runs

--- run.py
+++ run.py
@@ -5,16 +5,14 @@ from datetime import datetime
 
 app = create_app()
 
-# Add context processor to make current year available in all templates
+
 @app.context_processor
 def inject_now():
     return {'now': datetime.utcnow()}
 
 with app.app_context():
-    # Drop all existing tables and recreate them
     db.drop_all()
-    db.create_all()  # Uncommented this line to actually create tables
+    db.create_all()
 
 if __name__ == '__main__':
-    # Use the default port 5000 explicitly
     app.run(debug=True, port=5000)
